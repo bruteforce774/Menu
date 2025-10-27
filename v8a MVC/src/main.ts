5bde2d713b91067ac32980d8e3dd4c89033f62e9
Daniel Wang <danielaw@getacademy.no>
2025-10-27 13:01:52 +0100

    place customElements in right file

--- v8a MVC/src/main.ts
+++ v8a MVC/src/main.ts
@@ -1,12 +1,16 @@
+
 import { MenuView } from "./views/MenuView";
 import { MenuItemView } from "./views/MenuItemView";
 import { RouterView } from "./views/RouterView";
 import { AddCategoryView } from "./views/AddCategoryView";
 import { AddMenuItemView } from "./views/AddMenuItemView";
+import { AdminView } from "./views/AdminView";
 
 customElements.define('menu-item-view', MenuItemView);
 customElements.define('menu-view', MenuView);
 customElements.define('router-view', RouterView);
 customElements.define('add-category-view', AddCategoryView);
 customElements.define('add-menu-item-view', AddMenuItemView);
+customElements.define('admin-view', AdminView);
+
 
